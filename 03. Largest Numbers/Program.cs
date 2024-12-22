d3fe3a422c0e061b5e5ef657b7a4a1ba67ac03e4
Mariyan <116071995+MDimidov@users.noreply.github.com>
2024-12-22 18:01:16 +0200

    Solve 03. Largest Numbers

--- 03. Largest Numbers/Program.cs
+++ 03. Largest Numbers/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] intsArr = Console.ReadLine()
+    .Split(' ')
+    .Select(int.Parse)
+    .OrderByDescending(x => x)
+    .ToArray();
+
+int arrLength = int.Parse(Console.ReadLine()!);
+
+int[] result = intsArr
+    .Take(arrLength)
+    .OrderBy(x => x)
+    .ToArray();
+
+Console.WriteLine(string.Join(Environment.NewLine, result));
